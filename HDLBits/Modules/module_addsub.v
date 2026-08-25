8a2bb79b09f06362cda8667d2ba60889fdac0167
Dhruv Mittal <mittaldhruv41@gmail.com>
2026-08-25 23:08:51 +0530

    Solve module_addsub

---
+++ HDLBits/Modules/module_addsub.v
@@ -0,0 +1,19 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    input sub,
+    output [31:0] sum
+);
+    wire [31:0] exor;
+    wire cout;
+    wire [15:0] s1,s2;
+    
+    assign exor = {{32{sub}}^b};
+    
+    add16 add1(.a(a[15:0]), .b(exor[15:0]), .cin(sub), .cout(cout), .sum(s1));
+    add16 add2(.a(a[31:16]), .b(exor[31:16]), .cin(cout), .sum(s2));
+    
+    assign sum = {s2,s1};
+    
+
+endmodule
