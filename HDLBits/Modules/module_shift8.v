2a188aa66d5ab6851acce4eb5839f96fed67e9cd
Dhruv Mittal <mittaldhruv41@gmail.com>
2026-08-25 20:04:32 +0530

    Solve module_shift8

---
+++ HDLBits/Modules/module_shift8.v
@@ -0,0 +1,36 @@
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output [7:0] q 
+);
+    
+    wire [7:0] q1;
+    wire [7:0] q2;
+    wire [7:0] q3;
+    
+    my_dff8 dffA(.clk(clk),.d(d),.q(q1));
+    my_dff8 dffB(.clk(clk),.d(q1),.q(q2));
+    my_dff8 dffC(.clk(clk),.d(q2),.q(q3));
+    
+    
+    always @(*) begin
+    
+        if (sel == 2'b00) begin
+                 q <= d;
+        end else if (sel == 2'b01) begin
+                 q <= q1;
+        end else if(sel == 2'b10) begin
+                q <= q2;
+      	end else begin
+                q <= q3;
+        end
+
+    
+    end
+    
+    
+    
+    
+
+endmodule
